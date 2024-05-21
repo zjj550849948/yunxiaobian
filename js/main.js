49d863710430c739e388cd92171a456a1e447ee5
JingLei <550849948@qq.com>
2024-05-21 16:29:52 +0900

    Add files via upload
    更新0.2版本:
    1.今后版本号格式为主版本[0.2]后增加更新时间[24.05.21]以及当日第几次更新[.0].
    2.更改文件结构,将HTML文件中的css和js分离,并优化js相关逻辑.
    3.为css和js以及icon的链接方式添加日期版本.
    4.添加apple-touch-icon.
    5.添加404页面.
    6.在顶部菜单栏下方添加导航栏.
    7.点击"导航栏分类"或浏览到"分类内容"自动更新到"内容板块"的URL锚点.
    8.当浏览到相应"内容板块"时导航栏分类会切换到相应标签.
    9.通过带有锚点的URL打开页面将滚动到"内容板块"的高度.
    10.顶部"菜单栏"和"导航栏"固定在页面最上方.
    11."常用工具"板块下的"编辑页直达"现在支持在输入框内输入相关数字ID后按回车键打开相关页面.
    12.页面右下角添加"回到顶部".
    13.页面底部增加"版权申明"和"建议使用浏览器"提示.

---
+++ js/main.js
@@ -0,0 +1,148 @@
+// 编辑页面直达
+function createLink(baseURL, inputElementId, linkElementId) {
+    var userInput = document.getElementById(inputElementId).value;
+    var fullURL = baseURL + encodeURIComponent(userInput);
+    document.getElementById(linkElementId).href = fullURL;
+    window.open(fullURL);
+}
+
+// 回车进入编辑页
+function bindEnterKey(event, inputElementId, linkElementId, createFunction) {
+    if (event.key === "Enter") {
+        event.preventDefault(); // 阻止默认的回车键行为
+        createFunction();
+    }
+}
+
+// 初始化绑定
+bindEnterKey("songUserInput", "songLink", createSongLink);
+bindEnterKey("albumUserInput", "albumLink", createAlbumLink);
+bindEnterKey("artistUserInput", "artistLink", createArtistLink);
+bindEnterKey("addAlbumUserInput", "addAlbumLink", createAddAlbumLink);
+bindEnterKey("artistRepeatUserInput", "artistRepeatLink", createArtistRepeatLink);
+
+// 歌曲编辑页面直达
+function createSongLink() {
+    createLink("https://music.163.com/#/wiki/song?songId=", "songUserInput", "songLink");
+}
+
+// 专辑编辑页面直达
+function createAlbumLink() {
+    createLink("https://music.163.com/#/wiki/album?albumId=", "albumUserInput", "albumLink");
+}
+
+// 艺人编辑页面直达
+function createArtistLink() {
+    createLink("https://music.163.com/#/wiki/artist?artistId=", "artistUserInput", "artistLink");
+}
+
+// 艺人补充专辑页面直达
+function createAddAlbumLink() {
+    createLink("https://music.163.com/#/wiki/add-album?artistId=", "addAlbumUserInput", "addAlbumLink");
+}
+
+// 重复艺人报错页面直达
+function createArtistRepeatLink() {
+    createLink("https://music.163.com/#/wiki/artist-repeat-error?artistId=", "artistRepeatUserInput", "artistRepeatLink");
+}
+
+// 监听滚动事件以显示或隐藏回到顶部容器
+window.onscroll = function() {
+    if (window.scrollY > 100) {
+        document.querySelector(".back-to-top").classList.add("show"); // 添加显示按钮的类
+    } else {
+        document.querySelector(".back-to-top").classList.remove("show"); // 移除显示按钮的类
+    }
+};
+
+// 点击回到顶部按钮执行函数
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+    });
+}
+
+// 导航栏跳转修正
+document.addEventListener('DOMContentLoaded', function() {
+    // 获取导航栏和菜单的高度
+    var navHeight = document.querySelector('.nav') ? document.querySelector('.nav').offsetHeight : 0;
+    var menuHeight = document.querySelector('.menu') ? document.querySelector('.menu').offsetHeight : 0;
+    // 计算修正值
+    var correction = navHeight + menuHeight;
+
+    // 处理跳转事件
+    function scrollToElement(elementId) {
+        // 获取目标元素的位置
+        var element = document.getElementById(elementId);
+        if (element) {
+            var elementPosition = element.offsetTop - correction;
+
+            // 滚动到目标位置
+            window.scrollTo({
+                top: elementPosition,
+            });
+        }
+    }
+
+    // 检查 URL 中是否包含 hash，并滚动到相应位置
+    var hash = window.location.hash;
+    if (hash) {
+        var targetId = hash.slice(1);
+        scrollToElement(targetId);
+    }
+
+    // 找到所有的跳转链接并添加点击事件监听器
+    var links = document.querySelectorAll('a[href^="#"]');
+    links.forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault(); // 阻止默认的点击事件
+            var targetId = this.getAttribute('href').slice(1); // 获取目标元素的ID
+            scrollToElement(targetId); // 调用滚动函数
+
+            // 更新URL
+            history.pushState(null, null, '#' + targetId);
+        });
+    });
+
+    // 监听滚动事件，根据滚动位置更新URL并激活相应的导航链接
+    var sections = document.querySelectorAll('.container-header');
+    var sectionIds = Array.from(sections).map(function(section) {
+        return section.id;
+    });
+
+    window.addEventListener('scroll', function() {
+        var scrollPosition = window.scrollY + correction;
+        for (var i = 0; i < sections.length; i++) {
+            var section = sections[i];
+            var sectionTop = section.offsetTop;
+            var sectionBottom = sectionTop + section.offsetHeight;
+            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+                var sectionId = sectionIds[i];
+                if (window.location.hash !== '#' + sectionId) {
+                    history.replaceState(null, null, '#' + sectionId);
+                }
+
+                // 激活相应的导航链接
+                var navLinks = document.querySelectorAll('.nav .link');
+                navLinks.forEach(function(navLink) {
+                    if (navLink.getAttribute('href') === '#' + sectionId) {
+                        navLink.classList.add('active');
+                    } else {
+                        navLink.classList.remove('active');
+                    }
+                });
+
+                break;
+            }
+        }
+    });
+
+    // 延迟执行滚动到锚点位置的操作，以确保修正值已经被计算
+    setTimeout(function() {
+        var hash = window.location.hash;
+        if (hash) {
+            var targetId = hash.slice(1);
+            scrollToElement(targetId);
+        }
+    }, 100);
+});
